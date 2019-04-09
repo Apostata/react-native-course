8776537d53d9c891f4f2bcdf489b885b871d2fe0
tak_rsouza <tak_rsouza@uoldiveo.com>
2019-04-09 10:40:58 -0300

    [react-native] Commitendo antes de resolver estilos e responsividade para diferentes plataformas

--- styleEAnim/src/Screens/SideDrawer/SideDrawer.js
+++ styleEAnim/src/Screens/SideDrawer/SideDrawer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import {View, Text, Dimensions, StyleSheet } from 'react-native';
-
+import {View, Text, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const width = Dimensions.get('window').width * 0.7;
 
@@ -8,10 +8,13 @@ class SideDrawer extends Component{
    
     render(){
         return(
-            <View style={sideDrawerStyles.container}>
-                <Text>
-                    SideDrawer
-                </Text>
+            <View style={style.container}>
+                <TouchableOpacity>
+                    <View style={style.drawerItem}>
+                        <Icon style={style.drawerItemIcon} name="ios-log-out" size={30} color="#aaa"/>
+                        <Text>Deslogar-se</Text>
+                    </View>
+                </TouchableOpacity>
             </View>
         )
     }
@@ -19,12 +22,21 @@ class SideDrawer extends Component{
 
 
 export default SideDrawer;
-const sideDrawerStyles = StyleSheet.create({
+const style = StyleSheet.create({
     container:{
-        paddingTop: 22,
+        paddingTop: 50,
         width: width,
         backgroundColor: 'white',
         //height: '100%'
         flex: 1
+    },
+    drawerItem:{
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 10,
+        backgroundColor:"#eee"
+    },
+    drawerItemIcon:{
+        marginRight: 10
     }
 })
