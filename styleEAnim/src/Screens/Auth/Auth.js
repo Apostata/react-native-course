21d354c298eface24975b4f298d06aca411e5789
tak_rsouza <tak_rsouza@uoldiveo.com>
2019-04-10 10:56:30 -0300

    [react native] Adicionando responsividade via state

--- styleEAnim/src/Screens/Auth/Auth.js
+++ styleEAnim/src/Screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, ImageBackground } from 'react-native';
+import { View, StyleSheet, ImageBackground, Dimensions } from 'react-native';
 import Input from '../../components/UI/Input/Input';
 import Header from '../../components/UI/Header/Header';
 import MainText from '../../components/UI/MainText/MainText'
@@ -8,23 +8,64 @@ import startMainTabs from '../MainTabs/MainTabs';
 import backgroundImage from '../../assets/images/background.jpg';
 
 class AuthScreen extends Component {
+
+    state = {
+        resStyles:{
+            direction: "column",
+            justifyContent: 'flex-start',
+            width: '100%',
+            showHeader: true
+        }
+    };
+
+    constructor(props){
+        super(props);
+        Dimensions.addEventListener('change', dim =>{
+            this.setState({
+                resStyles:{
+                    direction: Dimensions.get('window').height < 500? "row" : "column",
+                    justifyContent: Dimensions.get('window').height < 500? 'space-between' : "flex-start",
+                    width: Dimensions.get('window').height < 500? '45%' : '100%',
+                    showHeader: Dimensions.get('window').height < 500? false : true
+                }
+            });
+        });
+    }
+
     loginHandler(){
         startMainTabs();
     }
 
     render(){
+        const {resStyles} = this.state;
+        let headingText = null;
+        if(resStyles.showHeader){
+            headingText = (
+                <MainText>
+                    <Header>Por favor, Logue-se</Header>
+                </MainText>
+            );
+        }
+
         return (
             <ImageBackground source={backgroundImage} style={styles.bgImage} >   
                 <View style={styles.container}>
-                        
-                        <MainText>
-                            <Header>Por favor, Logue-se</Header>
-                        </MainText>    
+                        {headingText}                            
                         <ButtonWithBg color="#29aaf4" onPress={this.loginHandler.bind(this)}>Logar-se</ButtonWithBg>
                         <View style={styles.inputContainer}>   
                             <Input placeholder="seuemail@domínio.com" style={styles.input} />
-                            <Input placeholder="Senha" style={styles.input} />
-                            <Input placeholder="Confirme sua senha" style={styles.input} />
+                            
+                            <View style={{
+                                flexDirection: resStyles.direction, 
+                                justifyContent: resStyles.justifyContent
+                            }}>
+                                <View style={{width: resStyles.width}}>
+                                    <Input placeholder="Senha" style={[styles.input, styles.inputInside]} />
+                                </View>
+                                <View style={{width: resStyles.width}}>
+                                    <Input placeholder="Confirme sua senha" style={[styles.input, styles.inputInside]} />
+                                </View>
+                            </View>
                         </View>
                         <ButtonWithBg color="#29aaf4">Enviar</ButtonWithBg>
                     
@@ -42,20 +83,36 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center"
     },
+    passwordContainer:{
+        flexDirection: Dimensions.get('window').height < 500 ? "row" : "column",
+        justifyContent: "space-between",
+    },
+
+    passwordWrapper:{
+        width:  Dimensions.get('window').height < 500 ? "45%" : "100%"
+    },
 
     bgImage:{
         width: "100%",
         flex: 1
     },
+
     text:{
         textAlign: 'center',
         width: '100%',
         marginBottom: 20
     },
+
     inputContainer:{
-        width: "80%"
+        width: "80%",
+        justifyContent: "space-between",
     },
+    
     input:{
        backgroundColor: "#eee"
-    }
+    },
+
+    // inputInside:{
+    //     width: "45%"
+    // }
 })
